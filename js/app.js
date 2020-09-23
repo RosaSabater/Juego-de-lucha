0cb661e33151b9d4b4b9b66b3abe9d987d2d2822
RosaCasa <rosamari.sab@gmail.com>
2020-09-23 08:18:35 +0200

    Created 'pulsaPersonaje' for choose one character

--- js/app.js
+++ js/app.js
@@ -18,55 +18,109 @@ const cambiaPantalla = (valor) => {
 
 
     // ahora sé a que pantalla quiero dirigirme al concatenar
-    let faseDestino= "fase" + valor;
+    let faseSiguiente= "fase" + valor;
 
     // creo un array con todas las fases
-    let arrayFases = ["fase1", "fase2", "fase3", "faseFinal"];
+    let arrayFases = ["fase1", "fase2", "fase3", "fase4"];
 
 
-    // incluir en arrayFaess todas las fases menos la de destino
-    //para ello usamos filter.
-    arrayFases = arrayFases.filter(val=> !faseDestino.includes(val));
+    // Si la fase es a la que voy no la oculto (la meto en pantallasOcultar), si no, la oculto.
+    let pantallasOcultar = arrayFases.filter(fase=> {
+        if (fase === faseSiguiente) {
+            return false;
+        }else {
+            return true;
+        }
+    });
 
-    //primero habilitamos la fase a la que uqeremos ir
-
-    document.getElementById(faseDestino).style.display = "block";
+    //primero habilitamos la fase a la que queremos ir
+    document.getElementById(faseSiguiente).style.display = "block";
 
     //finalmente deshabilitamos el resto
 
-    for(let pantalla of arrayFases) {
+    for(let pantalla of pantallasOcultar) {
         document.getElementById(pantalla).style.display ="none";
     }
 
 
 }
 
-// class Luchador {
+class Luchador {
 
-//     constructor (nombre, vida, fuerza, defensa, nacionalidad, suerte){
+    constructor (nombre, vida, fuerza, defensa, nacionalidad, suerte){
 
-//         this.nombre = nombre;
-//         this.vida = vida;
-//         this.fuerza = fuerza;
-//         this.defensa = defensa;
-//         this.nacionalidad = nacionalidad;
-//         this.suerte = suerte;
-//     }
-// }
+        this.nombre = nombre;
+        this.vida = vida;
+        this.fuerza = fuerza;
+        this.defensa = defensa;
+        this.nacionalidad = nacionalidad;
+        this.suerte = suerte;
+    }
+}
   
-// let player1 = new Luchador ("Arya Stark", 100, 70, 90, "Invernalia", 10);
+let Arya = new Luchador ("Arya Stark", 100, 70, 90, "Invernalia", 10);
 
-// let player2 = new Luchador ("Rey de los Caminantes Blancos", 100, 90, 40, "Más allá del muro", 6);
+let Rey = new Luchador ("Rey de los Caminantes Blancos", 100, 90, 40, "Más allá del muro", 6);
 
-// let player3 = new Luchador ("Daenerys Targaryen", 100, 100, 10, "Rocadragón", 8);
+let Daenerys = new Luchador ("Daenerys Targaryen", 100, 100, 10, "Rocadragón", 8);
 
-// let player4 = new Luchador ("Joffrey Baratheon", 100, 1, 1, "Desembarco del Rey", 1);
+let Joffrey = new Luchador ("Joffrey Baratheon", 100, 1, 1, "Desembarco del Rey", 1);
 
 
-// console.log("El luchador 1 tiene el siguiente estado:", player1)
+let personaje1 = document.getElementById("personaje1");
+let personaje2 = document.getElementById("personaje2");
+let personaje3 = document.getElementById("personaje3");
+let personaje4 = document.getElementById("personaje4");
 
-// console.log("El luchador 2 tiene el siguiente estado:", player2)
 
-// console.log("El luchador 3 tiene el siguiente estado:", player3)
+const pulsaPersonaje = (ev) => {
+    let seleccion = ev.target.id;
 
-// console.log("El luchador 4 tiene el siguiente estado:", player4)
+    if(escoger1 == "") { //si escoger1 está vacio, j1 no ha elegido todavía
+        escoger1 = seleccion; 
+
+        let textoSeleccion = document.getElementById("textoSeleccion");
+        textoSeleccion.innerText = "Jugador 2, elige personaje";
+    }else{
+        escoger2 = seleccion;
+        cambiaPantalla(3);
+    }
+}
+
+personaje1.addEventListener("click", pulsaPersonaje)
+personaje2.addEventListener("click", pulsaPersonaje)
+personaje3.addEventListener("click", pulsaPersonaje)
+personaje4.addEventListener("click", pulsaPersonaje)
+
+
+let escoger1 = "";
+let escoger2 = "";
+
+let textoSeleccion = document.getElementById("textoSeleccion");
+textoSeleccion.innerText = "Jugador 1, elige personaje";
+
+
+const idToPj = (id) => {
+    
+    // let nombrePersonaje;
+
+    switch (id) {
+
+        case "personaje1":
+            return Daenerys;
+        
+        case "personaje2":
+            return Arya;
+
+        case "personaje3":
+            return Joffrey;
+
+        case "personaje4":
+            return Rey;
+
+        default:
+            "No has escogido un personaje"
+        break;
+    }
+}
+
